cb170247fb3bf57d21801d30746063c1252a725a
nadeem-oka/ <nadeemokasha679@gmail.com>
2023-06-24 23:04:06 +0300

    fourth commit

--- final.py
+++ final.py
@@ -10,3 +10,10 @@ class Course:
         self.course_mark = course_mark
 class Student:
     total_students = 0
+
+    def __init__(self, student_name, student_age, student_number):
+        self.student_id = uuid.uuid4()
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = []
