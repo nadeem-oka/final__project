0a479a8adbf8d5b79ae4da7a3d438c0473e60e06
nadeem-oka/ <nadeemokasha679@gmail.com>
2023-06-24 22:58:50 +0300

    scound commit

--- final.py
+++ final.py
@@ -1,4 +1,10 @@
 #Name :nadeem okasha
 #Delivery Date :22-6-2023
 
+import uuid
 
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
