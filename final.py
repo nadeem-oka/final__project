b246ce400d72516a1d9c248e62a66eb6d021a83a
nadeem-oka/ <nadeemokasha679@gmail.com>
2023-06-26 23:43:45 +0300

    final

--- final.py
+++ final.py
@@ -8,6 +8,7 @@ class Course:
         self.course_id = uuid.uuid4()
         self.course_name = course_name
         self.course_mark = course_mark
+
 class Student:
     total_students = 0
 
@@ -34,6 +35,7 @@ class Student:
         return average
 
 students_list = []
+
 while True:
     try:
         selection = int(input("1.Add New Student\n"
@@ -55,7 +57,8 @@ while True:
                 break
             except ValueError:
                 print("Invalid input. Please enter a number.")
-existing_student = next((student for student in students_list if student.student_number == student_number), None)
+
+        existing_student = next((student for student in students_list if student.student_number == student_number), None)
         if existing_student:
             print("Student Number already exists. Please enter a unique Student Number.")
         else:
@@ -65,17 +68,16 @@ existing_student = next((student for student in students_list if student.student
 
     elif selection == 2:
         student_number = input("Enter Student Number: ")
+        existing_student = next((student for student in students_list if student.student_number == student_number), None)
+        if existing_student:
+            students_list.remove(existing_student)
+            print("Student Deleted Successfully")
+        else:
+            print("Student Not Exist")
 
-existing_student = next((student for student in students_list if student.student_number == student_number), None)
- if existing_student:
-    students_list.remove(existing_student)
-    print("Student Deleted Successfully")
- else:
-    print("Student Not Exist")
-
- elif selection == 3:
-student_number = input("Enter Student Number: ")
-existing_student = next((student for student in students_list if student.student_number == student_number), None)
+    elif selection == 3:
+        student_number = input("Enter Student Number: ")
+        existing_student = next((student for student in students_list if student.student_number == student_number), None)
         if existing_student:
             student_details = existing_student.get_student_details()
             print("Student Details:")
@@ -84,10 +86,9 @@ existing_student = next((student for student in students_list if student.student
         else:
             print("Student Not Exist")
 
-        elif selection == 4:
+    elif selection == 4:
         student_number = input("Enter Student Number: ")
-
-existing_student = next((student for student in students_list if student.student_number == student_number), None)
+        existing_student = next((student for student in students_list if student.student_number == student_number), None)
         if existing_student:
             average = existing_student.get_student_average()
             print(f"Student Average: {average}")
@@ -96,6 +97,12 @@ existing_student = next((student for student in students_list if student.student
 
     elif selection == 5:
         student_number = input("Enter Student Number: ")
-existing_student = next((student for student in students_list if student.student_number == student_number), None)
+        existing_student = next((student for student in students_list if student.student_number == student_number), None)
         if existing_student:
             course_name = input("Enter Course Name: ")
+            while True:
+                try:
+                    course_mark = float(input("Enter Course Mark: "))
+                    break
+                except ValueError:
+                    print("Invalid input.")
